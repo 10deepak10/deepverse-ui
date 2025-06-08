6569599ed99ea6a23cd85a9d0d79671770524ed5
deepak <mohantydeepak309@gmail.com>
2025-06-09 00:31:29 +0530

    dynamic table projection

--- projects/deepverse/ui/src/lib/table/table.ts
+++ projects/deepverse/ui/src/lib/table/table.ts
@@ -1,16 +1,16 @@
 import { AfterContentInit, Component, ContentChildren, Input, QueryList } from '@angular/core';
 import { TableItem } from './table-item/table-item';
 import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'dv-table',
-  standalone:true,
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './table.html',
-  styleUrl: './table.scss'
+  styleUrls: ['./table.scss']
 })
-
-export class Table implements AfterContentInit{
+export class Table implements AfterContentInit {
 
   @Input() rows: any[] = [];
 
@@ -19,7 +19,20 @@ export class Table implements AfterContentInit{
 
   columns: TableItem[] = [];
 
+  private columnDefsChangesSub?: Subscription;
+
   ngAfterContentInit() {
+    // Initial assignment
     this.columns = this.columnDefs.toArray();
+
+    // Subscribe for dynamic changes (e.g. via *ngIf, *ngFor)
+    this.columnDefsChangesSub = this.columnDefs.changes.subscribe(() => {
+      this.columns = this.columnDefs.toArray();
+    });
+  }
+
+  ngOnDestroy() {
+    // Clean up the subscription
+    this.columnDefsChangesSub?.unsubscribe();
   }
 }
