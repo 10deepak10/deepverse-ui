ccd41c05b9c2d577efe81a64b902a254f5742b40
deepak <mohantydeepak309@gmail.com>
2025-06-19 23:50:30 +0530

    sticky column functionality

--- projects/deepverse/ui/src/lib/table/table.ts
+++ projects/deepverse/ui/src/lib/table/table.ts
@@ -9,8 +9,9 @@ import {
   Input,
   QueryList,
   ViewChildren,
+  signal,
   WritableSignal,
-  signal
+  OnDestroy
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Subscription } from 'rxjs';
@@ -23,7 +24,7 @@ import { TableItem } from './table-item/table-item';
   templateUrl: './table.html',
   styleUrls: ['./table.scss']
 })
-export class Table implements AfterContentInit, AfterViewInit {
+export class Table implements AfterContentInit, AfterViewInit, OnDestroy {
   @Input() rows: any[] = [];
 
   @ContentChildren(TableItem) columnDefs!: QueryList<TableItem>;
@@ -34,7 +35,13 @@ export class Table implements AfterContentInit, AfterViewInit {
   stickyLeftOffsets: WritableSignal<number[]> = signal([]);
   stickyRightOffsets: WritableSignal<number[]> = signal([]);
 
+  colWidths: number[] = [];
+
   private columnDefsChangesSub?: Subscription;
+
+  private resizingColIndex: number | null = null;
+  private startX = 0;
+  private startWidth = 0;
 
   constructor(private cdr: ChangeDetectorRef) {}
 
@@ -43,56 +50,96 @@ export class Table implements AfterContentInit, AfterViewInit {
 
     this.columnDefsChangesSub = this.columnDefs.changes.subscribe(() => {
       this.columns = this.columnDefs.toArray();
-      requestAnimationFrame(() => this.computeStickyOffsets());
+      requestAnimationFrame(() => {
+        this.setInitialColWidths();
+        this.computeStickyOffsets();
+      });
     });
 
-    requestAnimationFrame(() => this.computeStickyOffsets());
+    requestAnimationFrame(() => {
+      this.setInitialColWidths();
+      this.computeStickyOffsets();
+    });
   }
 
   ngAfterViewInit() {
     this.thRefs.changes.subscribe(() => {
-      requestAnimationFrame(() => this.computeStickyOffsets());
+      requestAnimationFrame(() => {
+        this.setInitialColWidths();
+        this.computeStickyOffsets();
+      });
     });
 
-    requestAnimationFrame(() => this.computeStickyOffsets());
+    requestAnimationFrame(() => {
+      this.setInitialColWidths();
+      this.computeStickyOffsets();
+    });
+  }
+
+  setInitialColWidths() {
+    this.colWidths = this.thRefs.map((th) =>
+      th.nativeElement.getBoundingClientRect().width
+    );
   }
 
   computeStickyOffsets() {
     const leftOffsets: number[] = [];
     const rightOffsets: number[] = [];
-  
-    // Calculate left sticky offsets
+
+    // Left sticky
     let leftOffset = 0;
     for (let i = 0; i < this.columns.length; i++) {
       const col = this.columns[i];
       if (col.sticky === 'left') {
-        const th = this.thRefs.get(i)?.nativeElement;
-        const width = th?.getBoundingClientRect().width || 0;
         leftOffsets[i] = leftOffset;
-        leftOffset += width;
+        leftOffset += this.colWidths[i] || 0;
       } else {
         leftOffsets[i] = 0;
       }
     }
-  
-    // Calculate right sticky offsets
+
+    // Right sticky
     let rightOffset = 0;
     for (let i = this.columns.length - 1; i >= 0; i--) {
       const col = this.columns[i];
       if (col.sticky === 'right') {
-        const th = this.thRefs.get(i)?.nativeElement;
-        const width = th?.getBoundingClientRect().width || 0;
         rightOffsets[i] = rightOffset;
-        rightOffset += width;
+        rightOffset += this.colWidths[i] || 0;
       } else {
         rightOffsets[i] = 0;
       }
     }
-  
+
     this.stickyLeftOffsets.set(leftOffsets);
     this.stickyRightOffsets.set(rightOffsets);
     this.cdr.detectChanges();
   }
+
+  onResizeStart(event: MouseEvent, index: number) {
+    event.preventDefault();
+    this.resizingColIndex = index;
+    this.startX = event.clientX;
+    this.startWidth = this.colWidths[index];
+
+    document.addEventListener('mousemove', this.onMouseMove);
+    document.addEventListener('mouseup', this.onMouseUp);
+  }
+
+  onMouseMove = (event: MouseEvent) => {
+    if (this.resizingColIndex !== null) {
+      const dx = event.clientX - this.startX;
+      const newWidth = Math.max(this.startWidth + dx, 50); // minimum width
+      this.colWidths[this.resizingColIndex] = newWidth;
+      this.computeStickyOffsets();
+      this.cdr.detectChanges();
+    }
+  };
+
+  onMouseUp = () => {
+    this.resizingColIndex = null;
+    document.removeEventListener('mousemove', this.onMouseMove);
+    document.removeEventListener('mouseup', this.onMouseUp);
+  };
 
   ngOnDestroy() {
     this.columnDefsChangesSub?.unsubscribe();
