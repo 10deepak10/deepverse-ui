bc6626e05e2e37bfdbff01d169de9056c9d266bd
deepak <mohantydeepak309@gmail.com>
2025-07-06 19:36:07 +0530

    table

--- projects/documentation/src/app/component/tooltip/tooltip.ts
+++ projects/documentation/src/app/component/tooltip/tooltip.ts
@@ -1,61 +1,18 @@
 import { ChangeDetectionStrategy, Component, WritableSignal, signal } from '@angular/core';
 import { DvHeaderDirective, IconComponent, Table, TableItem, TooltipDirective ,DvCellDirective} from '@deepverse/ui';
+import dummyData from './dummy_employees.json';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tooltip',
-  imports: [TooltipDirective ,IconComponent , Table , TableItem , DvHeaderDirective , DvCellDirective],
+  imports: [ CommonModule, TooltipDirective ,IconComponent , Table , TableItem , DvHeaderDirective , DvCellDirective],
   templateUrl: './tooltip.html',
   styleUrl: './tooltip.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Tooltip {
   dynamic: WritableSignal<boolean> = signal(false)
-  rows = [
-    {
-      name: 'Alice Johnson',
-      age: 30,
-      role: 'Engineer',
-      email: 'alice@example.com',
-      dept: 'R&D',
-      location: 'NY',
-      startDate: '2020-01-15',
-      salary: 85000,
-      status: 'Active',
-      phone: '123-456-7890',
-      extension: 301,
-      manager: 'Bob Smith',
-      team: 'Backend',
-      bonus: 5000,
-      project: 'Apollo',
-      shift: 'Morning',
-      city: 'New York',
-      state: 'NY',
-      country: 'USA',
-      rating: 4.7,
-    },
-    {
-      name: 'Bob Smith',
-      age: 45,
-      role: 'Manager',
-      email: 'bob@example.com',
-      dept: 'Sales',
-      location: 'LA',
-      startDate: '2015-05-23',
-      salary: 105000,
-      status: 'Active',
-      phone: '234-567-8901',
-      extension: 205,
-      manager: '—',
-      team: 'Sales',
-      bonus: 8000,
-      project: 'Zeus',
-      shift: 'Evening',
-      city: 'Los Angeles',
-      state: 'CA',
-      country: 'USA',
-      rating: 4.9,
-    },
-  ];
+  rows = dummyData as any[];
 
   showAge = true;
   
