5928213570ccc89c85dba49949ecbc733d0376bf
deepak <mohantydeepak309@gmail.com>
2025-07-06 15:51:08 +0530

    sidebar updated

--- projects/deepverse/ui/src/lib/sidebar/sidebar.component.ts
+++ projects/deepverse/ui/src/lib/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { NavGroup } from '../utilites/interfaces';
@@ -9,19 +9,137 @@ import { NavGroup } from '../utilites/interfaces';
   imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
+  host: {
+    'class': 'sidebar-component',
+    '[class]': 'size == "sm" ? "sm" : ""',
+    '[class.expanded]': 'expandState !== "noExpand" && (sidebarExpand || expandState === "noCollapse" || (_showSidebar && viewport!=="desktop")) ',
+    '[class.fixCollapse]': 'expandState == "noExpand"',
+  }
 })
 export class SidebarComponent {
-  sidebarWidth = '12rem';
-  @Input() navGroups: NavGroup[] = [];
-  @Input() offset: string = ' 0rem';
-  @Input() activeColor: string = ' #012345';
-  @Input() bg: string = '#123456';
-  @Input() text: string = '#fff';
-  @Input() indicatorColor: string = '#aaffff';
-  Acolor: string = this.activeColor + '80';
-  hover: string = this.activeColor + '30';
-  toggleSidebarWidth() {
-    this.sidebarWidth = this.sidebarWidth == '12rem' ? '3rem' : '12rem';
+
+  @Input('config') config: any;
+  @Input('size') size: any // 'sm';
+  @Input('enableExpand') enableExpand = true;
+  private _showSidebar = false;
+  @Input('expandState') expandState : 'noExpand' | 'noCollapse' | 'default' = 'default';
+  @Input() set showSidebar( value : boolean)  {
+    if(value){
+      this._showSidebar = value;
+      this.updateBodyClass();
+    }
+    else{
+      this.renderer.removeClass(document.body, 'hide-scroll');
+    }
+  } //mobile sidebar
+
+  @Output() externalLinks = new EventEmitter<any>();
+  @Output() hideSidebar = new EventEmitter<any>();
+  @Output() expandEvent = new EventEmitter<any>();
+  @Output() logoEvent = new EventEmitter<any>();
+  @Input() sidebarExpand = false;
+  viewport: string = 'desktop';
+  isActive!: string;
+  isHovered = false;
+  constructor(
+    private el: ElementRef, private renderer: Renderer2
+  ) { 
+    this.renderer.listen('window', 'click',(e:Event)=>{
+      if(e.target == this.el.nativeElement ){
+        this.sidebarExpand = false;
+        this._showSidebar = false;
+        this.hideSidebar.emit(false);
+        this.expandEvent.emit(this.sidebarExpand)
+    }
+    })
   }
-  constructor(private route: Router) {}
+
+  toggleSidebar() {
+    this.sidebarExpand = !this.sidebarExpand;
+    this.expandEvent.emit(this.sidebarExpand);
+    if(!this.sidebarExpand){
+      this.closeAllChildren()
+;    }
+  }
+
+
+  closeAllChildren(){
+    this.config.menuItems.forEach((item: any) => {
+      if (item?.isExpand) {
+        item.isExpand = false;
+      }
+      if (item?.children) {
+        this.recursivelyClose(item.children);
+      }
+    });
+  }
+  
+  recursivelyClose(items: any[]) {
+    items.forEach((child: any) => {
+      child.isExpand = false;
+      if (child.children) {
+        this.recursivelyClose(child.children);
+      }
+    });
+  }
+  
+  
+
+  closeSidebar() {
+    if(this.viewport !== 'desktop'){
+      this.sidebarExpand = false;
+      this._showSidebar = false;
+      this.hideSidebar.emit(false);
+      this.expandEvent.emit(this.sidebarExpand)
+      this.renderer.removeClass(document.body, 'hide-scroll');
+    }
+    else {
+      if(this.sidebarExpand == false){
+        this.sidebarExpand = true;
+        this.expandEvent.emit(this.sidebarExpand)
+      }
+
+    };
+  }
+
+  ngOnInit(): void {
+    this.onWindowResize();
+  }
+  ngAfterViewInit() {
+    if (this.config) {
+      if (!this.config.iconPrefix) {
+        this.config.iconPrefix = '';
+      }
+    }
+
+  }
+
+  private updateBodyClass() {
+    if (this._showSidebar && this.viewport !=='desktop') {
+      this.renderer.addClass(document.body, 'hide-scroll');
+    } else {
+      this.renderer.removeClass(document.body, 'hide-scroll');
+    }
+  }
+
+
+  public customLinks(type:string) {
+    if (type) {
+      this.externalLinks.emit(type); 
+    this.isActive = type;
+    }
+    this.closeSidebar();
+  }
+
+  @HostListener("window:resize", ["$event"])
+  onWindowResize() {
+    if (window.innerWidth <= 767) {
+      this.viewport = "mobile";
+    } else if( window.innerWidth > 767 && window.innerWidth <= 992) {
+      this.viewport = "tablet";
+    }else{
+      this.viewport = "desktop";
+    }
+    this.updateBodyClass();
+  }
 }
--- projects/documentation/src/app/app.component.ts
+++ projects/documentation/src/app/app.component.ts
@@ -22,62 +22,318 @@ import {
     AvatarComponent,
     HeaderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
   ],
 })
 export class AppComponent implements OnInit {
-  
   title = 'documentation';
 
-  @ViewChild('userProfile', { static: true }) userProfileTemplate!: TemplateRef<any>;
+  @ViewChild('userProfile', { static: true })
+  userProfileTemplate!: TemplateRef<any>;
 
-  ngOnInit(): void {
+  config = {
+    hoverEffect: 'default',
+    menuItems: [
+      {
+        name: 'Dashboard',
+        link: '/accordion',
+        exact: true,
+        icon: 'assets/accordion.svg',
+        children: [
+          {
+            name: 'Admins',
+            link: '/users/admins',
+            icon: 'admin.svg',
+            children: [
+              {
+                name: 'Admins',
+                link: '/users/admins',
+                icon: 'admin.svg',
+                children: [
+                  {
+                    name: 'Admins',
+                    link: '/users/admins',
+                    icon: 'admin.svg',
+                  },
+                  {
+                    name: 'Clients',
+                    link: '/users/clients',
+                    icon: 'clients.svg',
+                  },
+                ],
+              },
+              {
+                name: 'Clients',
+                link: '/users/clients',
+                icon: 'clients.svg',
+                children: [
+                  {
+                    name: 'Admins',
+                    link: '/users/admins',
+                    icon: 'admin.svg',
+                  },
+                  {
+                    name: 'Clients',
+                    link: '/users/clients',
+                    icon: 'clients.svg',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: 'Clients',
+            link: '/users/clients',
+            icon: 'clients.svg',
+            children: [
+              {
+                name: 'Admins',
+                link: '/users/admins',
+                icon: 'admin.svg',
+              },
+              {
+                name: 'Clients',
+                link: '/users/clients',
+                icon: 'clients.svg',
+              },
+            ],
+          },
+          {
+            name: 'Admins',
+            link: '/users/admins',
+            icon: 'admin.svg',
+            children: [
+              {
+                name: 'Admins',
+                link: '/users/admins',
+                icon: 'admin.svg',
+                children: [
+                  {
+                    name: 'Admins',
+                    link: '/users/admins',
+                    icon: 'admin.svg',
+                  },
+                  {
+                    name: 'Clients',
+                    link: '/users/clients',
+                    icon: 'clients.svg',
+                  },
+                ],
+              },
+              {
+                name: 'Clients',
+                link: '/users/clients',
+                icon: 'clients.svg',
+                children: [
+                  {
+                    name: 'Admins',
+                    link: '/users/admins',
+                    icon: 'admin.svg',
+                  },
+                  {
+                    name: 'Clients',
+                    link: '/users/clients',
+                    icon: 'clients.svg',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: 'Admins',
+            link: '/users/admins',
+            icon: 'admin.svg',
+            children: [
+              {
+                name: 'Admins',
+                link: '/users/admins',
+                icon: 'admin.svg',
+                children: [
+                  {
+                    name: 'Admins',
+                    link: '/users/admins',
+                    icon: 'admin.svg',
+                  },
+                  {
+                    name: 'Clients',
+                    link: '/users/clients',
+                    icon: 'clients.svg',
+                  },
+                ],
+              },
+              {
+                name: 'Clients',
+                link: '/users/clients',
+                icon: 'clients.svg',
+                children: [
+                  {
+                    name: 'Admins',
+                    link: '/users/admins',
+                    icon: 'admin.svg',
+                  },
+                  {
+                    name: 'Clients',
+                    link: '/users/clients',
+                    icon: 'clients.svg',
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: 'Admins',
+            link: '/users/admins',
+            icon: 'admin.svg',
+            children: [
+              {
+                name: 'Admins',
+                link: '/users/admins',
+                icon: 'admin.svg',
+                children: [
+                  {
+                    name: 'Admins',
+                    link: '/users/admins',
+                    icon: 'admin.svg',
+                  },
+                  {
+                    name: 'Clients',
+                    link: '/users/clients',
+                    icon: 'clients.svg',
+                  },
+                ],
+              },
+              {
+                name: 'Clients',
+                link: '/users/clients',
+                icon: 'clients.svg',
+                children: [
+                  {
+                    name: 'Admins',
+                    link: '/users/admins',
+                    icon: 'admin.svg',
+                  },
+                  {
+                    name: 'Clients',
+                    link: '/users/clients',
+                    icon: 'clients.svg',
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: 'colors',
+        link: '/button',
+        exact: true,
+        icon: 'assets/button.svg',
+      },
+      {
+        name: 'colors',
+        link: '/button',
+        exact: true,
+        icon: 'assets/button.svg',
+      },
+      {
+        name: 'colors',
+        link: '/button',
+        exact: true,
+        icon: 'assets/button.svg',
+      },
+      {
+        name: 'colors',
+        link: '/button',
+        exact: true,
+        icon: 'assets/button.svg',
+      },
+      {
+        name: 'colors',
+        link: '/button',
+        exact: true,
+        icon: 'assets/button.svg',
+      },
+      {
+        name: 'colors',
+        link: '/button',
+        exact: true,
+        icon: 'assets/button.svg',
+      },
+      {
+        name: 'colors',
+        link: '/button',
+        exact: true,
+        icon: 'assets/button.svg',
+      },
+      {
+        name: 'colors',
+        link: '/button',
+        exact: true,
+        icon: 'assets/button.svg',
+      },
+      {
+        name: 'colors',
+        link: '/button',
+        exact: true,
+        icon: 'assets/button.svg',
+      },
+    ],
+    footerLinks: [
+      { name: 'How it works?', customLink: 'help', icon: 'assets/avatar.svg' },
+      { name: 'How it works?', customLink: 'help', icon: 'assets/avatar.svg' },
+      { name: 'How it works?', customLink: 'help', icon: 'assets/avatar.svg' },
+      { name: 'How it works?', customLink: 'help', icon: 'assets/avatar.svg' },
+    ],
+  };
+  sidbarToggle(e: any) {
+    console.log(e);
   }
 
+  ngOnInit(): void {}
 
   navGroup = [
     {
-      icon :'assets/home.svg',
+      icon: 'assets/home.svg',
       title: 'home',
       link: '',
     },
     {
-      icon :'assets/accordion.svg',
+      icon: 'assets/accordion.svg',
       title: 'accordion',
       link: 'accordion',
     },
     {
-      icon :'assets/alert.svg',
+      icon: 'assets/alert.svg',
       title: 'alert',
       link: 'alert',
     },
     {
-      icon :'assets/button.svg',
+      icon: 'assets/button.svg',
       title: 'button',
       link: 'button',
     },
     {
-      icon :'assets/avatar.svg',
+      icon: 'assets/avatar.svg',
       title: 'avatar',
       link: 'avatar',
     },
     {
-      icon :'assets/image-slider.svg',
+      icon: 'assets/image-slider.svg',
       title: 'image slider',
       link: 'image-slider',
     },
     {
-      icon :'assets/progress-bar.svg',
+      icon: 'assets/progress-bar.svg',
       title: 'progress bar',
       link: 'progress-bar',
     },
     {
-      icon :'assets/progress-bar.svg',
+      icon: 'assets/progress-bar.svg',
       title: 'image picker',
       link: 'imagepicker',
     },
     {
-      icon :'assets/progress-bar.svg',
+      icon: 'assets/progress-bar.svg',
       title: 'tooltip',
       link: 'tooltip',
     },
